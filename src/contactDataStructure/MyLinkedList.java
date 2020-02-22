5206394c2b047f073285b0abf4ffabef99c30b1d
Mayanksingh2004 <mayank.singh1_cs18@gla.ac.in>
2020-02-22 22:11:00 +0530

    Created MyLinkedList Class variables

--- src/contactDataStructure/MyLinkedList.java
+++ src/contactDataStructure/MyLinkedList.java
@@ -2,6 +2,13 @@ package contactDataStructure;
 
 public class MyLinkedList<E> implements MyLinkedListADT<E> {
 
+    private Node<E> head = null;
+    private int size = 0;
+
+    public Node<E> getHead() {
+        return head;
+    }
+
     @Override
     public void insert(E data) {
 
