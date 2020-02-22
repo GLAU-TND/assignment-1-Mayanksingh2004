58b0d6e032e25dd8015118307d4cc724564349e4
Mayanksingh2004 <mayank.singh1_cs18@gla.ac.in>
2020-02-22 21:53:06 +0530

    Declared interface functions

--- src/contactDataStructure/MyLinkedList.java
+++ src/contactDataStructure/MyLinkedList.java
@@ -1,5 +1,5 @@
 package contactDataStructure;
 
 public class MyLinkedList<E> {
-    private
+
 }
--- src/contactDataStructure/MyLinkedListADT.java
+++ src/contactDataStructure/MyLinkedListADT.java
@@ -1,4 +1,13 @@
 package contactDataStructure;
 
 public interface MyLinkedListADT {
+
+    void insert(int data);
+
+    void delete();
+
+    void traverse();
+
+    void sort();
+
 }
