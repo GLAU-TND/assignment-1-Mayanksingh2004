92accbeda2fef63a565fb4464cc2249ec54bb869
Mayanksingh2004 <mayank.singh1_cs18@gla.ac.in>
2020-02-22 21:51:29 +0530

    Created MyLinkedListADT Interface

--- src/contactDataStructure/MyLinkedList.java
+++ src/contactDataStructure/MyLinkedList.java
@@ -1,4 +1,5 @@
 package contactDataStructure;
 
-public class MyLinkedList {
+public class MyLinkedList<E> {
+    private
 }
---
+++ src/contactDataStructure/MyLinkedListADT.java
@@ -0,0 +1,4 @@
+package contactDataStructure;
+
+public interface MyLinkedListADT {
+}
