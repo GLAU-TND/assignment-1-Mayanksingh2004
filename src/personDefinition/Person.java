20c4c3efb2b21dea4b4fd8f25f6db056f7cf5d03
Mayanksingh2004 <mayank.singh1_cs18@gla.ac.in>
2020-02-22 22:07:57 +0530

    Created Person Class Variables

--- src/personDefinition/Person.java
+++ src/personDefinition/Person.java
@@ -1,4 +1,10 @@
 package personDefinition;
 
+import contactDataStructure.MyLinkedList;
+
 public class Person {
+    private String firstName;
+    private String lastName;
+    private MyLinkedList<Long> contactNumber;
+    private String emailAddress;
 }
