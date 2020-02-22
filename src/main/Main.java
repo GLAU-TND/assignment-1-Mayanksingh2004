c4f5b8cbeca96d3cac511775adb6e50367dc6c75
Mayanksingh2004 <mayank.singh1_cs18@gla.ac.in>
2020-02-22 22:35:41 +0530

    Main Class- Printed The Menu

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,5 +5,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean loop = false;
+        while (loop) {
+            System.out.println("Welcome to Mayank's Contact List App\n" +
+                    "Press 1 to add a new contact\n" +
+                    "Press 2 to view all contacts\n" +
+                    "Press 3 to search for a contact\n" +
+                    "Press 4 to delete a contact\n" +
+                    "Press 5 to exit program ");
+            int choice = scanner.nextInt();
+        }
     }
 }
