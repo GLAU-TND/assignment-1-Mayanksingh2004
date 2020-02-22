834935f436907c791eee7d447cdb1032f18610f2
Mayanksingh2004 <mayank.singh1_cs18@gla.ac.in>
2020-02-22 22:08:31 +0530

    Created Person Class Constructor

--- src/personDefinition/Person.java
+++ src/personDefinition/Person.java
@@ -7,4 +7,11 @@ public class Person {
     private String lastName;
     private MyLinkedList<Long> contactNumber;
     private String emailAddress;
+
+    public Person(String firstName, String lastName, MyLinkedList<Long> contactNumber, String emailAddress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contactNumber = contactNumber;
+        this.emailAddress = emailAddress;
+    }
 }
