2d01d55ac2d72d37af684ad41e2216a4f34c2c8d
Mayanksingh2004 <mayank.singh1_cs18@gla.ac.in>
2020-02-22 21:57:51 +0530

    Created Node Class Constructor

--- src/contactDataStructure/Node.java
+++ src/contactDataStructure/Node.java
@@ -3,4 +3,10 @@ package contactDataStructure;
 public class Node<E> {
     private E data;
     private Node<E> next;
+
+    public Node(E data, Node<E> next) {
+        this.data = data;
+        this.next = next;
+    }
+
 }
