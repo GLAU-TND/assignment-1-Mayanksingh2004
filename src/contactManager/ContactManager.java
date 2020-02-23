d1ed1a13aa1d2a4a04c9d1e3637a7f464fae6b82
Mayanksingh2004 <mayank.singh1_cs18@gla.ac.in>
2020-02-23 13:41:51 +0530

    Created Input Class - created contactNumber function to check if the number entered is valid or not

--- src/contactManager/ContactManager.java
+++ src/contactManager/ContactManager.java
@@ -1,14 +1,15 @@
 package contactManager;
 
 import contactDataStructure.MyLinkedList;
+import inputManager.Input;
 import personDefinition.Person;
 
 import java.util.Scanner;
 
 public class ContactManager {
     Scanner scanner = new Scanner(System.in);
-    MyLinkedList<Long> contactNubList = null;
-
+    MyLinkedList<Long> contactNumberList = null;
+    Input input = new Input();
     Person contact = null;
 
 }
--- src/inputManager/Input.java
+++ src/inputManager/Input.java
@@ -1,4 +1,13 @@
 package inputManager;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Input {
+    public boolean contactNumber(long number) {
+        String pattern = "^(91|0)?[1-9][0-9]{9}$";
+        Pattern pattern1 = Pattern.compile(pattern);
+        Matcher matcher = pattern1.matcher(String.valueOf(number));
+        return matcher.matches();
+    }
 }
