5031f4aa55a662da8aed12d0bb01e591c6777292
Mayanksingh2004 <mayank.singh1_cs18@gla.ac.in>
2020-02-23 13:45:25 +0530

    ContactManager Class - created addContact function to insert new entries into LinkedList

--- src/contactManager/ContactManager.java
+++ src/contactManager/ContactManager.java
@@ -12,4 +12,42 @@ public class ContactManager {
     Input input = new Input();
     Person contact = null;
 
+    public Person addContact() {
+        System.out.print("You have chosen to add a new contact: \n" +
+                "Please enter the name of the Person\n" +
+                "First Name: ");
+        String firstName = scanner.nextLine();
+        System.out.print("Last Name : ");
+        String lastName = scanner.nextLine();
+        // contactNumberList
+        contactNumberList = new MyLinkedList();
+        char choice = 'y';
+        long contactNumber;
+        while (choice == 'y') {
+            System.out.print("Contact Number :");
+            contactNumber = Long.parseLong(scanner.nextLine());
+            if (input.contactNumber(contactNumber)) {
+                contactNumberList.insert(contactNumber);
+            } else {
+                System.out.println("Invalid Contact Number");
+                continue;
+            }
+
+            System.out.print("Would you like to add another contact number? (y/n): ");
+            choice = (scanner.nextLine()).charAt(0);
+        }
+        System.out.print("Email Address: ");
+        String emailAddress = scanner.nextLine();
+        while (true) {
+            if (input.emailAddress(emailAddress)) {
+                contact = new Person(firstName, lastName, contactNumberList, emailAddress);
+                break;
+            } else {
+                System.out.println("Invalid Email address");
+                System.out.print("Email Address: ");
+                emailAddress = scanner.nextLine();
+            }
+        }
+        return contact;
+    }
 }
